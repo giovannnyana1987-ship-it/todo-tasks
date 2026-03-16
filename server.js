bc1b8867124b4c6f060d8a8bd2888df59922475e
Anderson <giovannny-a1987@hotmail.com>
2026-03-16 18:00:36 -0500

    Conexion BD OK, Variables de entorno

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 const express = require('express');
+const api = require('./api');
 const port = process.env.PORT || 3000;
 const app = express();
+
+const bosyParser = require('body-parser');
+app.use(bosyParser.json());
 
 app.listen(port, function () {
     console.log("Server is listening at port: " + port);
@@ -9,3 +16,5 @@ app.listen(port, function () {
 app.get('/', function (req, res) {
     res.send("hello world");
 });
+
+app.use('/api', api);
